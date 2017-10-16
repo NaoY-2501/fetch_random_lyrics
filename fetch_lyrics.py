647f491b25c39d008a89bc23ac7971ed37246471
NaoY-2501 <solaris2501.fe2@gmail.com>
2017-10-17 00:37:18 +0900

    issue#1: Input name of artist who wants to fetch lyrics as args.

--- fetch_lyrics.py
+++ fetch_lyrics.py
@@ -1,3 +1,4 @@
+import argparse
 import random
 from collections import defaultdict
 
@@ -13,13 +14,15 @@ def get_html(url):
         print('error')
 
 
-def parse_song_list(content):
+def parse_song_list(content, artist_name):
+    artist_name = artist_name.replace('-', ' ')
     soup = bs(content, 'html.parser')
     a = soup.find_all('a', class_='title hasvidtable')
     songs_dict = defaultdict(str)
     for each in a:
-        if 'oasis' in (each['title']).lower():
+        if artist_name in (each['title']).lower():
             songs_dict[each.get_text()] = each['href']
+    print(songs_dict)
     return songs_dict
 
 
@@ -30,13 +33,28 @@ def parse_lyric(content):
         print(each.get_text())
 
 
+def create_parser():
+    parser = argparse.ArgumentParser(description='Fetch lyric randomly')
+    parser.add_argument(
+        'artist_name',
+        metavar='ARTIST NAME',
+        type=str,
+        nargs='*',
+        help='Name of the artist who wants to fetch lyrics.'
+    )
+    return parser
+
+
 def main():
-    keyword = 'oasis'
+    parser = create_parser()
+    args = parser.parse_args()
+    artist_name = '-'.join([word.lower() for word in args.artist_name])
+
     url = 'http://www.metrolyrics.com/{keyword}-lyrics.html'.format(
-        keyword=keyword
+        keyword=artist_name
     )
     content = get_html(url)
-    songs_dict = parse_song_list(content)
+    songs_dict = parse_song_list(content, artist_name)
 
     random_song = random.choice([song for song in songs_dict.keys()])
     random_song_url = songs_dict.get(random_song)
